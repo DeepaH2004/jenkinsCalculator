85003878bc3a1cbb4fbdd4dc5419b873ff3d1225
Deepa S H <deepahebballi4@gmail.com>
2024-02-20 10:37:24 +0530

    code for Extendedcalculator operations

---
+++ Extendedcalculator.java
@@ -0,0 +1,13 @@
+public class Extendedcalculator{
+    float square(float operand_1) {
+        return (operand_1 * operand_1);
+    }
+
+    float cube(float operand_1) {
+        return (operand_1 * operand_1 * operand_1);
+    
+    
+}
+}
+
+
